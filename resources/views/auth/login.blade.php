fadc521a3c9960307dd63434aa70022b247daa4e
Tanner Cox <tannercox@outlook.com>
2017-08-06 20:21:21 -0600

    fix pages, add circleci integration

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
-
+@section('title')
+    User Login
+@stop
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col m8 offset-m2">
-                <div class="card medium">
+                <div class="card">
                     <div class="card-content">
-                        <div class="card-title center-align">Login</div>
+                        <div class="card-title">Login</div>
                         <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
                             {{ csrf_field() }}
-                            <div>
+                            <div class="row">
                                 <div class="{{ $errors->has('email') ? 'has-error' : '' }} col m6 input-field s12">
-                                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                    <label for="email" class="control-label">E-Mail Address</label>
                                     <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
                                     @if ($errors->has('email'))
                                         <span class="help-block">
@@ -20,7 +22,7 @@
                                     @endif
                                 </div>
                                 <div class="{{ $errors->has('password') ? 'has-error' : '' }} col m6 input-field s12">
-                                    <label for="password" class="col-md-4 control-label">Password</label>
+                                    <label for="password" class="control-label">Password</label>
                                     <input id="password" type="password" class="form-control" name="password" required>
                                     @if ($errors->has('password'))
                                         <span class="help-block">
@@ -45,7 +47,6 @@
                                         <a onclick="$('form').submit()" class="btn btn-primary waves-effect waves-light">
                                             Login
                                         </a>
-                                        <br>
                                         <a class="btn btn-flat" href="{{ route('password.request') }}">
                                             Forgot Your Password?
                                         </a>
