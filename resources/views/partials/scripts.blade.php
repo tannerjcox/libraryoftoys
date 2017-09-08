60fda9f7fa79af01afbc915984085c5616e63f47
Tanner Cox <tannercox@outlook.com>
2017-09-07 22:31:51 -0600

    quick fix

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,6 +1,6 @@
 <script src="{{ asset('vendor/js/jquery-3.2.1.min.js') }}"></script>
 <script src="{{ asset('vendor/materialize/js/materialize.min.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
-@if(app()->environment() == 'production')
+@if(app()->environment() == 'prod')
     <script src="{{ asset('js/ga.js') }}"></script>
 @endif
