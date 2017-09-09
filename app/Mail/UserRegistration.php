7919903e35c4d3900d78d8f292175f3a267d5fbe
Tanner Cox <tannercox@outlook.com>
2017-09-08 21:55:23 -0600

    Registration mailer

---
+++ app/Mail/UserRegistration.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserRegistration extends Mailable
+{
+    use Queueable, SerializesModels;
+    private $userName;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($userName)
+    {
+        $this->userName = $userName;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.user-registration', [
+            'userName' => $this->userName
+        ]);
+    }
+}
---
+++ resources/views/mail/user-registration.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.mail')
+@section('title')
+    ToyTrader Registration
+@stop
+@section('content')
+    <p>Hi {{ $userName }},</p>
+    <p>Thank you for registering with ToyTrader! We are excited to provide a service where you can connect with people who want to rent toys to and from you! Our service is free, and we hope you enjoy trading toys!</p>
+    <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+        <tbody>
+        <tr>
+            <td align="center">
+                <table border="0" cellpadding="0" cellspacing="0">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <div style="padding:15px;">
+                                <a href="{{ route('products.create') }}" target="_blank">List A Toy</a>
+                            </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+@stop
